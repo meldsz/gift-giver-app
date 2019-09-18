39c7fea6762eb19d4673e2dfde91757095530599
Meleesha <mayola.dsz@gmail.com>
2019-09-18 23:24:48 +0100

    added App component using TDD approach

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Button } from "react-bootstrap";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            gifts: []
+        }
+    }
+
+    addGift = () => {
+        const { gifts } = this.state;
+        const ids = gifts.map(gift => gift.id);
+        const maxId = gifts.length > 0 ? Math.max(...ids) : 0;
+        gifts.push({ id: maxId + 1 });
+        this.setState({ gifts });
+    }
+    render() {
+        return (
+            <div>
+                <h2>Gift Giver</h2>
+                <div className="gift-list">
+                    {this.state.gifts.map(gift =>
+                        <div key={gift.id}></div>
+                    )}
+                </div>
+                <Button onClick={this.addGift}>Add Gift</Button>
+            </div>
+        )
+    }
+}
+
+export default App;
